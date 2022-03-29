3ef11847df56f460999cd7333337abeb856b2f0f
Amr Abdelsamee <67904417+Amr-abdelsamee@users.noreply.github.com>
2022-03-30 01:12:53 +0200

    Update circles.py
    none used function removed

--- circles.py
+++ circles.py
@@ -1,4 +1,3 @@
-from matplotlib.pyplot import draw
 import pygame
 
 class Circle:
@@ -18,33 +17,3 @@ class Circle:
     def update(self,color):
         self.color = color
 
-    # check_clicked checks if the mouse click was on the block 
-    def check_clicked(self, x_clicked , y_clicked):
-        if( x_clicked >= self.x_pos 
-        and x_clicked < self.x_pos + self.width
-        and y_clicked >= self.y_pos
-        and y_clicked < self.y_pos + self.height):
-            return True
-        
-    
-    # getters and setters for the class
-    def get_label(self):
-        return self.label
-    
-    def get_position(self):
-        return self.position
-
-    def get_x_pos(self):
-        return self.x_pos
-    
-    def get_y_pos(self):
-        return self.y_pos
-
-    def set_x_pos(self, new_x_pos):
-        self.x_pos = new_x_pos
-    
-    def set_y_pos(self, new_y_pos):
-        self.y_pos = new_y_pos
-    
-    def set_position(self, new_position):
-        self.position = new_position
