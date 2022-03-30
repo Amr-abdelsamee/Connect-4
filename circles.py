a4cde1763665093c58bbb9d1ee4245c89693b654
Amr Abdelsamee <67904417+Amr-abdelsamee@users.noreply.github.com>
2022-03-30 02:02:55 +0200

    Update circles.py
    update function can now draw when called

--- circles.py
+++ circles.py
@@ -14,6 +14,7 @@ class Circle:
         pygame.draw.circle(screen, self.color,(self.x_pos, self.y_pos), self.radius)
     
     # update used to update the block place called when exchange happens
-    def update(self,color):
+    def update(self, screen, color):
         self.color = color
+        self.draw(screen)
 
