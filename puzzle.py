1afe7259a97b94a2c933df6c41cf8e81ceb32604
Amr Abdelsamee <67904417+Amr-abdelsamee@users.noreply.github.com>
2022-03-31 00:22:05 +0200

    throw added
    switched the name of play function to throw
    play function now  is used to switch turns between players

--- puzzle.py
+++ puzzle.py
@@ -10,7 +10,8 @@ INBTWN_SPACE = 1
 
 # blocks constants
 CIRCLE_COLOR = (155,45,205)
-PLAYED_CIRCLE_COLOR = (12, 90, 55)
+PLAYER1_CIRCLE_COLOR = (12, 90, 55)
+PLAYER2_CIRCLE_COLOR = (120, 9, 5)
 
 class Puzzle:
 
@@ -25,10 +26,15 @@ class Puzzle:
         self.screen_height = screen_height
         self.num_row = num_row
         self.num_col = num_col
+
+        self.player_turn = 1
         
         # calculate the block width and height depending on the screen width and height
         self.diameter = (self.screen_width - (2 * SIDES_PADDING) - (
                     self.num_col * INBTWN_SPACE - 1)) / self.num_col
+        # self.diameter2 = (self.screen_height - LOWER_PADDING - UPPER_PADDING - (
+        #             self.num_row * INBTWN_SPACE - 1)) / self.num_row
+        # self.diameter = (self.diameter1 + self.diameter2)//2
 
         self.create_circles()
         self.generate_playable()
@@ -38,16 +44,14 @@ class Puzzle:
         # initial cordinates of the first block
         x = SIDES_PADDING + self.diameter/2
         y = UPPER_PADDING + self.diameter/2
-        position = 0
 
         for _ in range(0, self.num_row):
             # self.clickable_ranges.append((x-self.diameter/2, y-self.diameter/2))
             for _ in range(0, self.num_col):
-                circle = Circle(x, y, CIRCLE_COLOR, position, self.diameter/2)
+                circle = Circle(x, y, CIRCLE_COLOR, self.diameter/2)
                 circle.draw(self.screen)
                 self.circles.append(circle)
                 x = x + self.diameter + INBTWN_SPACE
-                position += 1
             x = SIDES_PADDING + self.diameter/2
             y = y + self.diameter + INBTWN_SPACE
 
@@ -64,7 +68,6 @@ class Puzzle:
     def get_col_clicked(self, x_clicked, y_clicked):
         last_circle_index = (self.num_col * self.num_row) -1
         y_end = self.circles[last_circle_index].y_pos + (self.diameter/2)
-
         for i in range(self.num_col):
             x_start = self.circles[i].x_pos - (self.diameter/2)
             x_end = self.circles[i].x_pos + (self.diameter/2)
@@ -75,11 +78,31 @@ class Puzzle:
             and y_clicked < y_end):
                 return i
 
-    def play(self, x_clicked, y_clicked):
+
+    def throw(self, x_clicked, y_clicked, color, owner):
         col_index = self.get_col_clicked(x_clicked, y_clicked)
         if col_index != None:
             if self.playable[col_index]:
                 circle_index = max(self.playable[col_index])
                 self.playable[col_index].remove(circle_index)
                 self.occupied.append(circle_index)
-                self.circles[circle_index].update(self.screen, PLAYED_CIRCLE_COLOR)
+
+                self.circles[circle_index].update(self.screen, color, owner)
+
+    def get_score():
+        pass
+
+
+    def play(self, x_clicked, y_clicked):
+        if len(self.occupied) == self.num_col*self.num_row:
+            print("calc score")
+            self.get_score()
+
+        elif self.player_turn == 1:
+            self.throw(x_clicked, y_clicked, PLAYER1_CIRCLE_COLOR, '1')
+            self.player_turn = 2
+        elif self.player_turn == 2:
+            self.throw(x_clicked, y_clicked, PLAYER2_CIRCLE_COLOR, '2')
+            self.player_turn = 1
+        
+
