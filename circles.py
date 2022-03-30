14fcb1e7d0f7d309fa47d44590f560dfb6ecad5c
Amr Abdelsamee <67904417+Amr-abdelsamee@users.noreply.github.com>
2022-03-31 00:23:16 +0200

    Update circles.py
    owner player  variable added
    update function can now update the owner of the circle

--- circles.py
+++ circles.py
@@ -1,12 +1,12 @@
 import pygame
 
 class Circle:
-    def __init__(self, x_pos, y_pos, color, position_index, radius):
+    def __init__(self, x_pos, y_pos, color, radius):
         
         self.x_pos = x_pos
         self.y_pos = y_pos
         self.color = color
-        self.position_index = position_index
+        self.player = None
         self.radius = radius
         
     # draw used to draw the block
@@ -14,7 +14,8 @@ class Circle:
         pygame.draw.circle(screen, self.color,(self.x_pos, self.y_pos), self.radius)
     
     # update used to update the block place called when exchange happens
-    def update(self, screen, color):
+    def update(self, screen, color, owner):
         self.color = color
+        self.player = owner
         self.draw(screen)
 
