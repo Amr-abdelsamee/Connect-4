9114ff3a9ce7445177437d51227b3308b2fc5ebb
Amr Abdelsamee <67904417+Amr-abdelsamee@users.noreply.github.com>
2022-04-01 18:21:22 +0200

    Update main.py
    a moving circle added indicates which player should play

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-from time import sleep
 from tkinter import *
 from PIL import ImageTk, Image
 import pygame
+import sys
 from puzzle import Puzzle
+from circles import Circle
 from tree import Tree
+from copy import copy
 
 NUM_ROW = 6
 NUM_COL = 7
 
 # size constants
 SCREEN_WIDTH = 900
-SCREEN_HEIGHT = SCREEN_WIDTH
+SCREEN_HEIGHT = SCREEN_WIDTH + 50
 
+WHITE = (255, 255, 255)
+BG_COLOR = WHITE
 # back ground color constant
-BGROUND_IMG = pygame.image.load("BG.jpg")
+# BGROUND_IMG = pygame.image.load("BG.jpg")
 
 pygame.init()
 
@@ -38,24 +42,26 @@ def message(image_name):
     tree_screen.mainloop()
 
 
-    # tree_screen.withdraw()
-
 def tree_window(states):
-    states = [["123",["1st_state_child1","1st_state_child2","1st_state_child3","1st_state_child4"]],
-            ["456",["2nd_state_child1","2nd_state_child2","2nd_state_child3","2nd_state_child4"]],
-            ["789",["3rd_state_child"]]
-            ]
-    tree = Tree(states)
+    # states = [["1",["1st_state_child1","1st_state_child2","1st_state_child3","1st_state_child4"]],
+    #         ["2",["2nd_state_child1","2nd_state_child2","2nd_state_child3","2nd_state_child4"]],
+    #         ["3",["3rd_state_child"]],
+    #         ["4",["4th_state_child1","4th_state_child3","4th_state_child4"]],
+    #         ["1st_state_child2",["5th_state_child1","4th_state_child3"]],
+    #         ["6",["6th_state_child1","4th_state_child3"]],
+    #         ["7",["7th_state_child1","4th_state_child3"]]
+    #         ]
+    tree = Tree(states, NUM_COL, NUM_ROW)
     image_name = tree.png_name +'.'+tree.extension
     message(image_name)
-
 
 
 # starting the pygame screen
 game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # draw the background 
-game_screen.blit(BGROUND_IMG,(0,0))
+game_screen.fill(BG_COLOR)
+# game_screen.blit(BGROUND_IMG,(0,0))
 # set the title
 pygame.display.set_caption("Connect 4")
 # set the logo
@@ -67,20 +73,30 @@ pygame.display.set_icon(icon)
 
 puzzle = Puzzle(game_screen, NUM_ROW, NUM_COL, SCREEN_WIDTH, SCREEN_HEIGHT)
 
-running = True # running condition
+playing_circle = Circle(game_screen, puzzle.circles[0].x_pos, puzzle.circles[0].y_pos - puzzle.diameter-10, puzzle.player1_color, puzzle.diameter/2)
+playing_circle.draw()
 
-while running:
+running = True # running condition
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-        # check if user clicked
+            pygame.quit()
+            sys.exit()
+
+        if event.type == pygame.MOUSEMOTION:
+            x_hovered, y_hovered = pygame.mouse.get_pos()
+            clear_rect = pygame.Rect(0, 0, SCREEN_WIDTH, 140)
+            pygame.draw.rect(game_screen, BG_COLOR, clear_rect)
+            playing_circle.change_pos(x_hovered, puzzle.circles[0].y_pos - puzzle.diameter-10)
+            
         if event.type == pygame.MOUSEBUTTONDOWN:
             # store the coordinates of the clicked position
             x_clicked, y_clicked = pygame.mouse.get_pos()
             
             puzzle.play(x_clicked , y_clicked)
+            if puzzle.player_turn == '2': playing_circle.update(puzzle.player2_color, '0')
+            else: playing_circle.update(puzzle.player1_color, '0')
             pygame.display.update()
-            tree_window(puzzle.state)
+            # if puzzle.player_turn == '1': tree_window(copy(puzzle.states))
 
         pygame.display.update()
-pygame.quit()
